e41c81f95f86bd3a92e9a7af1600167e517149ad
Amir <amir_rajabifar@yahoo.com.au>
2016-06-10 17:37:14 +0800

    Out put text

---
+++ OutPuttingText/OutPuttingText.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Stating programm.." << flush; //out put the text but don't end the line
+	cout << "This is some text" << endl;	//out put the text and end the line
+	cout << "This is some more text." << endl;
+	cout << "Bannana," << " Apple," << " Orange." << endl;
+	return 0;
+}
