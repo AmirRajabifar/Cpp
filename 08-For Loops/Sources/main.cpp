e7edb468ac7c754ee4e228cc3ec7fe5237accc3b
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 23:35:27 +0800

    For Loops

---
+++ 08-For Loops/Sources/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int count = 20;
+	for (int i = 0; i < count; ++i)
+	{
+		cout << "Loop # --> " << i + 1 << endl;
+	}
+	return 0;
+}
