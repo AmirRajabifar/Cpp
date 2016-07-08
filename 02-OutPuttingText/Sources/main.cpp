57f2e127d61b65d51d90c8e32c1bdad6fe391612
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 21:37:58 +0800

    More on \'cout\'

--- 02-OutPuttingText/Sources/main.cpp
+++ 02-OutPuttingText/Sources/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	int age = 23;
+	float average = 32.53;
 	cout << "Starting programm.." << flush; //out put the text but don't end the line
 	cout << "This is some text" << endl;	//out put the text and end the line
 	cout << "This is some more text." << endl;
 	cout << "Bannana," << " Apple," << " Orange." << endl;
+	cout << "Your age is: " << age << endl << "Average is: " << average << endl;
 	return 0;
 }
