a98f586bfb642b7d426a48acdf968eef4371574e
Amir <amir_rajabifar@yahoo.com.au>
2016-07-08 21:30:52 +0800

    Add file structure

--- 02-OutPuttingText/Sources/main.cpp
+++ 02-OutPuttingText/Sources/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout << "Stating programm.." << flush; //out put the text but don't end the line
+	cout << "Starting programm.." << flush; //out put the text but don't end the line
 	cout << "This is some text" << endl;	//out put the text and end the line
 	cout << "This is some more text." << endl;
 	cout << "Bannana," << " Apple," << " Orange." << endl;
