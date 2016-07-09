4546fc1a63784e89130e1870a989be9b24428889
Amir <amir_rajabifar@yahoo.com.au>
2016-07-09 23:50:10 +0800

    in-line function

--- 11-Functions/Sources/main.cpp
+++ 11-Functions/Sources/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+
+//inline functions are used for really short functions
+
+inline void add (int a, int b)
+{
+	cout << a + b << endl;
+}
+
 int main(int argc, char const *argv[])
 {
 	cout << "Hello from main\n";
@@ -13,5 +21,7 @@ int main(int argc, char const *argv[])
 
 	default_func(11, 20); //Third parameter is defualt (unchanged)
 	default_func(5, 4, 32);
+
+	add(25, 15); //in-line function
 	return 0;
 }
