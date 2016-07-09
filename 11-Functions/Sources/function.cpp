b3f925b3a55605fac682377b45b79cb9d68e7c81
Amir <amir_rajabifar@yahoo.com.au>
2016-07-09 22:57:17 +0800

    Functions

---
+++ 11-Functions/Sources/function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+void display()
+{
+	cout << "Hello from display" << endl;
+}
---
+++ 11-Functions/Sources/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hello from main\n";
+	display();
+	
+	return 0;
+}
