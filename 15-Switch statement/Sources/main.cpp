1d6c471ddb253ff8d011a7dc28c142b63f3d9031
Amir <amir_rajabifar@yahoo.com.au>
2016-07-10 20:10:43 +0800

    Switch statement

---
+++ 15-Switch statement/Sources/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	char value = 0;
+	cout << "Enter your grade (A, B, C, ...): " << flush;
+	cin >> value;
+	switch (value)
+	{
+		case 'A':
+		case 'a':
+			cout << "Excellent" << endl;
+			break;
+		case 'B':
+		case 'b':
+			cout << "Very good" << endl;
+			break;
+		case 'C':
+		case 'c':
+			cout << "good" << endl;
+			break;
+		case 'D':
+		case 'd':
+			cout << "Not bad" << endl;
+			break;
+		default:
+			cout << ":`(" << endl;
+	}	
+	return 0;
+}
