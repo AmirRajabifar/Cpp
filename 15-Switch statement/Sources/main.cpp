6af5dd01a3508cca96edaf366fd715f3ffc191cb
Amir <amir_rajabifar@yahoo.com.au>
2016-07-29 20:13:28 +0800

    Add while loop

--- 15-Switch statement/Sources/main.cpp
+++ 15-Switch statement/Sources/main.cpp
@@ -5,28 +5,31 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	char value = 0;
-	cout << "Enter your grade (A, B, C, ...): " << flush;
-	cin >> value;
-	switch (value)
+	while (1)
 	{
-		case 'A':
-		case 'a':
-			cout << "Excellent" << endl;
-			break;
-		case 'B':
-		case 'b':
-			cout << "Very good" << endl;
-			break;
-		case 'C':
-		case 'c':
-			cout << "good" << endl;
-			break;
-		case 'D':
-		case 'd':
-			cout << "Not bad" << endl;
-			break;
-		default:
-			cout << ":`(" << endl;
+		cout << "Enter your grade (A, B, C, ...): " << flush;
+		cin >> value;
+		switch (value)
+		{
+			case 'A':
+			case 'a':
+				cout << "Excellent" << endl;
+				break;
+			case 'B':
+			case 'b':
+				cout << "Very good" << endl;
+				break;
+			case 'C':
+			case 'c':
+				cout << "good" << endl;
+				break;
+			case 'D':
+			case 'd':
+				cout << "Not bad" << endl;
+				break;
+			default:
+				cout << ":`(" << endl;
+		}
 	}	
 	return 0;
 }
